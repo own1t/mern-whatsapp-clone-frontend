bbee639d876b7482d2c6fc93382a372cb827220a
Ryan Kim <iawal86@gmail.com>
2020-09-19 15:17:03 -0700

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBiX82yV99qUhNANRY4qomHB6_aP5x0ex0",
+  authDomain: "whatsapp-clone-9490e.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-9490e.firebaseio.com",
+  projectId: "whatsapp-clone-9490e",
+  storageBucket: "whatsapp-clone-9490e.appspot.com",
+  messagingSenderId: "889463199499",
+  appId: "1:889463199499:web:a389846e8476bccf19b40e",
+};
