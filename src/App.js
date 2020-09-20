9e2dccc4180c0a5ce7b197a8ffeba0457c46b273
Ryan Kim <iawal86@gmail.com>
2020-09-19 17:55:17 -0700

    created pusher

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 // React
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+// Pusher
+import Pusher from "pusher-js";
 
 // CSS
 import "./App.css";
@@ -8,7 +11,37 @@ import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Chat from "./views/Chat";
 
+// Axios
+import axios from "./axios";
+
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/messages").then((res) => {
+      setMessages(res.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    const pusher = new Pusher("cb6ab0b0f095d65082cd", {
+      cluster: "us3",
+    });
+
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", (newMessage) => {
+      alert(JSON.stringify(newMessage));
+      setMessages([...messages, newMessage]);
+    });
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages]);
+
+  console.log(messages);
+
   return (
     <div className="app">
       <div className="app__body">
